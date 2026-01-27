a24795b76e101a8a646607782d4648e7b60bfe26
Nemanja <nemanja92@icloud.com>
2026-01-26 19:20:22 -0500

    Initial Commit

---
+++ ScreenHub/ScreenHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenHubApp.swift
+//  ScreenHub
+//
+//  Created by Nemanja Ignjatovic on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
